9707e183eb86a518f95698cbd03260a2d6e40518
yapi <yapi.tw@gmail.com>
2019-02-06 01:32:05 +0800

    add lib 4

---
+++ src/labs/4/index.ts
@@ -0,0 +1,119 @@
+import * as THREE from 'three'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector<HTMLDivElement>('#app').style.filter = 'saturate(0) brightness(1.4) contrast(2)'
+})
+
+const texture = new THREE.TextureLoader().load('/doodle.png')
+
+interface IUniforms {
+  u_time: { type: 'f'; value: number }
+  u_resolution: { type: 'v2'; value: THREE.Vector2 }
+  u_mouse: { type: 'v2'; value: THREE.Vector2 }
+  u_texture: { type: 't'; value: THREE.Texture }
+  u_picture: { type: 't'; value: THREE.Texture }
+  u_mousedown: { type: 'bool'; value: boolean }
+}
+
+class Lab {
+  camera: THREE.OrthographicCamera
+  scene: THREE.Scene
+  renderer: THREE.WebGLRenderer
+  canvas: HTMLCanvasElement
+  uniforms: IUniforms
+  textBuffer1: THREE.WebGLRenderTarget
+  textBuffer2: THREE.WebGLRenderTarget
+  switchTag: boolean
+  pixelRatio: number
+  renderSize: number
+  constructor() {
+    this.init = this.init.bind(this)
+    this.animation = this.animation.bind(this)
+    this.init()
+    this.animation()
+  }
+  init() {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.OrthographicCamera(-2, 2, -2, 2)
+    this.renderer = new THREE.WebGLRenderer({ alpha: true })
+    this.pixelRatio = 1
+    this.renderSize = 512
+    this.switchTag = false
+
+    const { scene, camera, renderer, pixelRatio, renderSize } = this
+
+    renderer.setSize(renderSize, renderSize)
+    renderer.setPixelRatio(pixelRatio)
+    document.getElementById('app').appendChild(renderer.domElement)
+
+    camera.position.set(1, 1, 1)
+    camera.lookAt(0, 0, 0)
+
+    this.canvas = document.querySelector('canvas')
+    const geometry = new THREE.PlaneBufferGeometry(2, 2)
+    this.uniforms = {
+      u_time: { type: 'f', value: 0 },
+      u_resolution: { type: 'v2', value: new THREE.Vector2() },
+      u_mouse: { type: 'v2', value: new THREE.Vector2() },
+      u_texture: { type: 't', value: undefined },
+      u_picture: { type: 't', value: texture },
+      u_mousedown: { type: 'bool', value: false },
+    }
+
+    var material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: require('./shaderVertex.glsl'),
+      fragmentShader: require('./shaderFragment.glsl'),
+    })
+
+    var mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+    const targetOptions = {
+      minFilter: THREE.LinearFilter,
+      magFilter: THREE.LinearFilter,
+      format: THREE.RGBAFormat,
+      // type: THREE.FloatType,
+      type: THREE.HalfFloatType, // for ios compatibility
+      wrapS: THREE.RepeatWrapping,
+      wrapT: THREE.RepeatWrapping,
+    }
+    this.textBuffer1 = new THREE.WebGLRenderTarget(renderSize * pixelRatio, renderSize * pixelRatio, targetOptions)
+    this.textBuffer2 = new THREE.WebGLRenderTarget(renderSize * pixelRatio, renderSize * pixelRatio, targetOptions)
+
+    this.uniforms.u_resolution.value.x = renderSize * pixelRatio
+    this.uniforms.u_resolution.value.y = renderSize * pixelRatio
+
+    const container = document.querySelector<HTMLDivElement>('#app')
+    this.canvas.onmousemove = e => {
+      const x = (e.pageX - container.offsetLeft) / this.canvas.clientWidth
+      const y = 1 - (e.pageY - container.offsetTop) / this.canvas.clientHeight
+      this.uniforms.u_mouse.value.x = x
+      this.uniforms.u_mouse.value.y = y
+      // console.log(`x:${x}, y:${y}`)
+    }
+
+    this.canvas.onmousedown = e => {
+      this.uniforms.u_mousedown.value = true
+    }
+
+    this.canvas.onmouseup = e => {
+      this.uniforms.u_mousedown.value = false
+    }
+  }
+  animation() {
+    const { scene, camera, renderer } = this
+    for (let i = 0; i < 12; i++) {
+      this.uniforms.u_texture.value = this[this.switchTag ? 'textBuffer1' : 'textBuffer2'].texture
+      renderer.render(scene, camera, this[this.switchTag ? 'textBuffer2' : 'textBuffer1'], true)
+      this.uniforms.u_texture.value = this[this.switchTag ? 'textBuffer2' : 'textBuffer1'].texture
+      this.switchTag = !this.switchTag
+      this.uniforms.u_time.value += 1
+    }
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(this.animation)
+  }
+}
+
+export { Lab }
---
+++ src/labs/4/shaderFragment.glsl
@@ -0,0 +1,70 @@
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform sampler2D u_texture;
+uniform sampler2D u_picture;
+uniform bool u_mousedown;
+
+float Da = 1.0;
+float Db = 0.5;
+float feedRate = 0.0545;
+float killRate = 0.062;
+float deltaT = 1.;
+
+bool pixelate(float value, float num, float step) {
+  return (value >= num - step && value <= num + step);
+}
+
+void main() {
+
+  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+
+  float step_x = 1. / u_resolution.x;
+  float step_y = 1. / u_resolution.y;
+
+  vec2 p = texture2D(u_texture, uv).rg;
+  vec2 p1 = texture2D(u_texture, uv + vec2(-step_x, step_y)).rg;
+  vec2 p2 = texture2D(u_texture, uv + vec2(0., step_y)).rg;
+  vec2 p3 = texture2D(u_texture, uv + vec2(step_x, step_y)).rg;
+  vec2 p4 = texture2D(u_texture, uv + vec2(-step_x, 0.)).rg;
+  vec2 p5 = texture2D(u_texture, uv).rg;
+  vec2 p6 = texture2D(u_texture, uv + vec2(step_x, 0.)).rg;
+  vec2 p7 = texture2D(u_texture, uv + vec2(-step_x, -step_y)).rg;
+  vec2 p8 = texture2D(u_texture, uv + vec2(0., -step_y)).rg;
+  vec2 p9 = texture2D(u_texture, uv + vec2(step_x, -step_y)).rg;
+
+  vec2 deltaP = (p1 * vec2(.05, .05) + p2 * vec2(.2, .2) + p3 * vec2(.05, .05) +
+                 p4 * vec2(.2, .2) + p5 * vec2(-1., -1.) + p6 * vec2(.2, .2) +
+                 p7 * vec2(.05, .05) + p8 * vec2(.2, .2) + p9 * vec2(.05, .05));
+
+  float chemicalA =
+      p.r + (Da * deltaP.r - p.r * p.g * p.g + feedRate * (1. - p.r)) * deltaT;
+
+  float chemicalB =
+      p.g +
+      (Db * deltaP.g + p.r * p.g * p.g - (killRate + feedRate) * p.g) * deltaT;
+
+  vec2 chemical = vec2(chemicalA, chemicalB);
+
+  if (u_time == 0.) {
+    chemical.r = 1.0;
+    // if (pixelate(uv.x, 0.1, 5. * step_x) && pixelate(uv.y, 0.1, 5. * step_y))
+    // {
+    //   chemical.g = 1.0;
+    // };
+  } else {
+
+    if (u_mousedown) {
+      float pointDistance =
+          pow((uv.x - u_mouse.x), 2.) + pow((uv.y - u_mouse.y), 2.);
+      pointDistance = pow(pointDistance, 0.5);
+      if (pointDistance < 0.002) {
+        chemical.g = 1.0;
+        chemical.r = 0.0;
+      }
+    }
+  }
+
+  // vec2 st = gl_FragCoord.xy / u_resolution.xy;
+  gl_FragColor = vec4(chemical.x, chemical.y, 0., 1.0);
+}
